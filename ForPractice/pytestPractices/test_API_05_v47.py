9586dc9d83137fd74624495f96c4ac98393ff624
grupovalma <grupovalma@gmail.com>
2026-09-22 18:17:55 -0400

    API TESTING Play-Python 47 - 50

--- ForPractice/pytestPractices/test_API_05_v47.py
+++
@@ -1,8 +0,0 @@
-import time
-
-from playwright.sync_api import Page, expect, Playwright
-
-
-def test_API01(page: Page):
-    page.goto("https://rahulshettyacademy.com/client/#/auth/login")
-    time.sleep(15)
---
+++ ForPractice/pytestPractices/test_web_api_5_vid48.py
@@ -0,0 +1,18 @@
+import time
+
+from playwright.sync_api import Playwright
+
+def test_e2e_web_api(playwright:Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    #Create Order
+    page.goto("https://rahulshettyacademy.com/client")
+    page.get_by_placeholder("email@example.com").fill("grupovalma@gmail.com")
+    page.get_by_placeholder("enter your passsword").fill("NewPassword01")
+    page.get_by_role("button", name="login").click()
+
+    #Orders History Page -> order is present
+
+
