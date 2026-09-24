1ae68c4ffbdb1a1061adb11e6ea0c5ed347d2b47
grupovalma <grupovalma@gmail.com>
2026-09-24 19:10:24 -0400

    Leccion 1 API Completa

--- ForPractice/pytestPractices/test_web_api_5_vid48.py
+++ ForPractice/pytestPractices/test_web_api_5_vid48.py
@@ -1,21 +1,33 @@
-from playwright.sync_api import Playwright
+from playwright.sync_api import Playwright, expect
 
-from ForPractice.pytestPractices.utils.apiBase import APIUtils
+from utils.apiBase import APIUtils
+
 
 def test_e2e_web_api(playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
-    #Create Order
+    #Create Order - Creamos el producto a traves del API en apiBase.py, tambien se imprime
     api_utils = APIUtils()
+    orderId = api_utils.createOrder(playwright)
 
-    #Login
+    #Login - Entramos a la pagina, para verificar el producto por UI
     page.goto("https://rahulshettyacademy.com/client")
-    page.get_by_placeholder("email@example.com").fill("grupovalma@gmail.com")
+    page.locator("#userEmail").fill("grupovalma@gmail.com")
     page.get_by_placeholder("enter your passsword").fill("NewPassword01")
     page.get_by_role("button", name="login").click()
 
-    #Orders History Page -> order is present
+    #Entramos a las ordenes por UI
+    page.get_by_role("button", name="ORDERS").click()
+
+    #Orders History Page -> order is present  , Verificamos por UI si el producto fue insertado por el API, y hacemos un Assertion
+    row = page.locator("tr").filter(has_text=orderId)
+    row.get_by_role("button", name="View").click()
+    expect(page.locator(".tagline")).to_contain_text("Thank you for Shopping With Us")
+    context.close()
+
+
+
 
 
